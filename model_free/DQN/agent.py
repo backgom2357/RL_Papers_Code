43c181cf5c320d73e16e119b469b5aa400c81908
Diominor <backgom2357@gmail.com>
2020-09-03 18:20:01 +0900

    update agent.py

--- model_free/DQN/agent.py
+++ model_free/DQN/agent.py
@@ -19,14 +19,14 @@ class Agent:
         self.action_dim = action_dim
 
         # Setting Replay Memory
-        self.rm = ReplayMemory(self.cf.replay_memory_size, self.cf.frame_size, self.cf.agent_history_length)
+        self.rm = ReplayMemory(self.cf.REPLAY_MEMORY_SIZE, self.cf.FRAME_SIZE, self.cf.AGENT_HISTORY_LENGHTH)
         
         # Build Model
-        self.q = build_model(self.cf.frame_size, self.action_dim, self.cf.agent_history_length)
-        self.target_q = build_model(self.cf.frame_size, self.action_dim, self.cf.agent_history_length)
+        self.q = build_model(self.cf.FRAME_SIZE, self.action_dim, self.cf.AGENT_HISTORY_LENGHTH)
+        self.target_q = build_model(self.cf.FRAME_SIZE, self.action_dim, self.cf.AGENT_HISTORY_LENGHTH)
 
         # Optimizer and Loss for Training
-        self.optimizer=tf.keras.optimizers.Adam(learning_rate=self.cf.learning_rate, clipnorm=10.)
+        self.optimizer=tf.keras.optimizers.Adam(learning_rate=self.cf.LEARNING_RATE, clipnorm=10.)
         self.loss = tf.keras.losses.Huber()
         self.q.summary()
 
@@ -35,15 +35,15 @@ class Agent:
         Epsilon Greedy
         """
         q = self.q(state)[0]
-        return np.argmax(q), q if self.cf.epsilon < np.random.rand() else np.random.randint(self.action_dim), q
+        return np.argmax(q), q if self.cf.EPSILON < np.random.rand() else np.random.randint(self.action_dim), q
 
     def model_train(self):
         # Sample From Replay Memory
-        states, actions, rewards, next_states, dones = self.replay_memory.sample(self.cf.batch_size)
+        states, actions, rewards, next_states, dones = self.replay_memory.sample(self.cf.BATCH_SIZE)
 
-        # Epsilon Decay
-        if self.cf.epsilon > self.cf.final_exploration:
-            self.cf.epsilon -= (self.cf.initial_exploration + self.cf.final_exploration)/(self.cf.final_exploration_frame*self.cf.epsilon)
+        # Epsilon Decay (+ exponentially)
+        if self.cf.EPSILON > self.cf.FINAL_EXPLORATION:
+            self.cf.EPSILON -= (1 + self.cf.FINAL_EXPLORATION)/(self.cf.FINAL_EXPLORATION_FRAME*self.cf.EPSILON)
         
         # Update Weights
         with tf.GradientTape() as g:
@@ -51,9 +51,11 @@ class Agent:
             max_next_q = np.max(self.target_q(next_states), axis=1)
 
             # Calculate Targets
-            targets = rewards + (1 - dones) * (self.cf.discount_factor * max_next_q)
+            targets = rewards + (1 - dones) * (self.cf.DISCOUNT_FACTOR * max_next_q)
             predicts = self.q(states)
+            predicts = tf.reduce_sum(predicts * tf.one_hot(actions, self.action_dim), axis=1)
             loss = self.loss(targets, predicts)
+            loss = np.clip(loss, -1, 1)
         g_theta = g.gradient(loss, self.q.trainable_weights)
         self.optimizer.apply_gradients(g_theta, self.q.trainable_weights)
 
@@ -67,13 +69,13 @@ class Agent:
         episode = 0
         frames, action = 0, 0
         initial_state = self.evn.reset()
-        state = np.stack([preprocess(initial_state, frame_size=self.cf.frame_size)]*4, axis=3)
+        state = np.stack([preprocess(initial_state, frame_size=self.cf.FRAME_SIZE)]*4, axis=3)
         state = np.reshape(state, (1, *state.shape))
 
         # No Ops
         for _ in range(self.cf.no_ops):
             next_state, _, _,  _ = self.env.step(0)
-            next_state = np.append(state[..., 1:], preprocess(next_state, frame_size=self.cf.frame_size), axis=3))
+            next_state = np.append(state[..., 1:], preprocess(next_state, frame_size=self.cf.FRAME_SIZE), axis=3)
             state = next_state
 
         while frames < max_frame:
@@ -85,13 +87,13 @@ class Agent:
             action, q = self.get_action(normalize(state))
             state, reward, done, _ = self.env.step(action)
             reward = np.clip(reward, -1, 1)
-            next_state = np.append(state[..., 1:], preprocess(next_state, frame_size=self.cf.frame_size), axis=3))
+            next_state = np.append(state[..., 1:], preprocess(next_state, frame_size=self.cf.FRAME_SIZE), axis=3)
 
             # Append To Replay Memeory
             self.replay_memory.append(state, action, reward, next_state, done)
 
             # Start Training After Collecting Enough Samples
-            if self.relay_memeory.crt < self.cf.replay_start_size and not self.replay_memory.is_full():
+            if self.relay_memeory.crt < self.cf.REPLAY_START_SIZE and not self.replay_memory.is_full():
                 state = next_state
                 continue
             
@@ -105,19 +107,19 @@ class Agent:
             frames += 1
 
             # Update Target Q
-            if frames % self.cf.target_q_update_frequency  == 0:
+            if frames % self.cf.TARGET_NETWORK_UPDATE_FREQUENCY  == 0:
                 self.target_q.set_weights(self.q.get_weights())
 
             if done:
-                episodic_mean_q = sum_mean_q/frames(self.cf.skip_frames+1)
+                episodic_mean_q = sum_mean_q/frames(self.cf.SKIP_FRAMES + 1)
                 episode += 1
 
                 # Update Logs
                 print(f'Epi : {self.episode,}, Reward : {episodic_rewards}, Q : {episodic_mean_q}')
-                wandb.log({
-                    'Reward':episode_reward, 
-                    'Q value':episodic_mean_q, 
-                    })
+                # wandb.log({
+                #     'Reward':episode_reward, 
+                #     'Q value':episodic_mean_q, 
+                #     })
 
                 # Save Model
                 if episode % 1000 == 0:
@@ -127,13 +129,9 @@ class Agent:
                 sum_mean_q, episodic_rewards = 0, 0
                 action = 0
                 initial_state = self.evn.reset()
-                state = np.stack([preprocess(initial_state, frame_size=self.cf.frame_size)]*4, axis=3)
+                state = np.stack([preprocess(initial_state, frame_size=self.cf.FRAME_SIZE)]*4, axis=3)
                 state = np.reshape(state, (1, *state.shape))
-                for _ in range(self.cf.no_ops):
+                for _ in range(self.cf.NO_OPS):
                     next_state, _, _,  _ = self.env.step(0)
-                    next_state = np.append(state[..., 1:], preprocess(next_state, frame_size=self.cf.frame_size), axis=3))
+                    next_state = np.append(state[..., 1:], preprocess(next_state, frame_size=self.cf.FRAME_SIZE), axis=3)
                     state = next_state
-
-                
-        
-        
