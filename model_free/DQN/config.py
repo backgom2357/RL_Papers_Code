a1d9f1e59f599f599dcdd94f143fe1604da26ebc
Diominor <backgom2357@gmail.com>
2020-09-03 18:20:09 +0900

    update config.py

--- model_free/DQN/config.py
+++ model_free/DQN/config.py
@@ -8,3 +8,19 @@ class Config():
             3:'SeaquestDeterministic-v0',
             4:'SpaceInvadersDeterministic-v0'
             }
+        
+        self.FRAME_SIZE = 84
+        self.REPLAY_MEMORY_SIZE = 1000000
+        self.AGENT_HISTORY_LENGHTH = 4
+        self.LEARNING_RATE = 0.00025
+        self.epsilon = 1
+        self.BATCH_SIZE = 32
+        self.FINAL_EXPLORATION = 0.1
+        self.FINAL_EXPLORATION_FRAME = 1000000
+        self.DISCOUNT_FACTOR = 0.99
+        self.REPLAY_START_SIZE = 50000
+        self.TARGET_NETWORK_UPDATE_FREQUENCY = 10000
+        self.SKIP_FRAMES = 4
+        self.NO_OPS = 30
+
+        self.wandb = {'FRAME_SIZE':self.FRAME_SIZE}
